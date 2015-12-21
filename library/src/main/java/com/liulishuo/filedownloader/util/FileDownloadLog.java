3d8d3cf2794edf251196180a5618d160de16c17a
Jacksgong <igzhenjie@gmail.com>
2015-12-21 20:20:14 +0800

    优化FileDownload log结构

--- library/src/main/java/com/liulishuo/filedownloader/util/FileDownloadLog.java
+++ library/src/main/java/com/liulishuo/filedownloader/util/FileDownloadLog.java
@@ -7,29 +7,50 @@ import android.util.Log;
  */
 public class FileDownloadLog {
 
-    private final static String TAG = "FileDownloader";
+    public static boolean NEED_LOG = false;
+
+    private final static String TAG = "FileDownloader.";
 
     public static void e(Object o, Throwable e, String msg, Object... args) {
-        Log.e(TAG + o.getClass().getSimpleName(), String.format(msg, args), e);
+        log(Log.ERROR, o, e, msg, args);
     }
 
     public static void e(Object o, String msg, Object... args) {
-        Log.e(TAG + o.getClass().getSimpleName(), String.format(msg, args));
+        log(Log.ERROR, o, msg, args);
     }
 
     public static void i(Object o, String msg, Object... args) {
-        Log.i(TAG + o.getClass().getSimpleName(), String.format(msg, args));
+        log(Log.INFO, o, msg, args);
     }
 
     public static void d(Object o, String msg, Object... args) {
-        Log.d(TAG + o.getClass().getSimpleName(), String.format(msg, args));
+        log(Log.DEBUG, o, msg, args);
     }
 
     public static void w(Object o, String msg, Object... args) {
-        Log.w(TAG + o.getClass().getSimpleName(), String.format(msg, args));
+        log(Log.WARN, o, msg, args);
     }
 
     public static void v(Object o, String msg, Object... args) {
-        Log.v(TAG + o.getClass().getSimpleName(), String.format(msg, args));
+        log(Log.VERBOSE, o, msg, args);
+    }
+
+    private static void log(int priority, Object o, String message, Object... args) {
+        log(priority, o, null, message, args);
+    }
+
+    private static void log(int priority, Object o, Throwable throwable, String message, Object... args) {
+        if (!NEED_LOG) {
+            return;
+        }
+
+        Log.println(priority, getTag(o), String.format(message, args));
+        if (throwable != null) {
+            throwable.printStackTrace();
+        }
+    }
+
+    private static String getTag(final Object o) {
+        return TAG + ((o instanceof Class) ? ((Class) o).getSimpleName() : o.getClass().getSimpleName());
     }
 }
