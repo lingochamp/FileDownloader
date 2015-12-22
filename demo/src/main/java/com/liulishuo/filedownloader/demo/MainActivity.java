c1069a3ffe40dc8afc1f63abca1fc3043c6b4dbf
Jacksgong <igzhenjie@gmail.com>
2015-12-22 19:35:13 +0800

    demo中添加github url

--- demo/src/main/java/com/liulishuo/filedownloader/demo/MainActivity.java
+++ demo/src/main/java/com/liulishuo/filedownloader/demo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.liulishuo.filedownloader.demo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -25,4 +28,26 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, SingleTaskActivity.class));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_github:
+                openGitHub();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openGitHub() {
+        Uri uri = Uri.parse(getString(R.string.app_github_url));
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
 }
