4f293e1311d9f792231150df10ac99de87f9b860
Jacksgong <igzhenjie@gmail.com>
2016-01-30 00:14:25 +0800

    test(Download Url): delete some unused download url in Constant.java Closes #21

--- demo/src/main/java/com/liulishuo/filedownloader/demo/Constant.java
+++ demo/src/main/java/com/liulishuo/filedownloader/demo/Constant.java
@@ -36,8 +36,6 @@ public interface Constant {
             "http://www.pc6.com/down.asp?id=72873",
             // 22m
             "http://113.207.16.84/dd.myapp.com/16891/2E53C25B6BC55D3330AB85A1B7B57485.apk?mkey=5630b43973f537cf&f=cf87&fsname=com.htshuo.htsg_3.0.1_49.apk&asr=02f1&p=.apk",
-            // 34.2m
-            "https://cx-ep.xiaojukeji.com/pkg//download/passenger/taxi/newhome/android/output/passenger_android_build_release/351/DiDi_passenger_taxi_newhome_build_351.apk?_t=hl6KNbYOuMLom9tRRV0U43DSMpjwXAXmhorB84U6ajlmmRigEsXBr9qEAjOeMJIVAajJAqwhrLMdpJaffBj4o6kgvZ/A3iUTlgkYq4RPdhzDWNJ1tIbHlqdeC9PEu5ISYCMfa%2BiRPyPJQ3c0qcLuhcNbmXtYp3v%2BvIn3vIWFNSM= ",
             // 206m
             "http://down.tech.sina.com.cn/download/d_load.php?d_id=49535&down_id=1&ip=42.81.45.159",
     };
