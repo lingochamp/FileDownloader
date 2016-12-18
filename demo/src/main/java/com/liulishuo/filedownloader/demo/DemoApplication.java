d22f28a620d5d55afa8223c703c033963585a89a
Jacksgong <igzhenjie@gmail.com>
2016-12-18 16:24:43 +0800

    demo(adapt-no-okhttp): adapt there is no okhttp dependency as default anymore in the library Refs #158

--- demo/src/main/java/com/liulishuo/filedownloader/demo/DemoApplication.java
+++ demo/src/main/java/com/liulishuo/filedownloader/demo/DemoApplication.java
@@ -5,17 +5,12 @@ import android.content.Context;
 import android.util.Log;
 
 import com.liulishuo.filedownloader.FileDownloader;
-import com.liulishuo.filedownloader.util.FileDownloadHelper;
 import com.liulishuo.filedownloader.util.FileDownloadLog;
 import com.liulishuo.filedownloader.util.FileDownloadUtils;
-
-import java.net.Proxy;
-import java.util.concurrent.TimeUnit;
 
 import cn.dreamtobe.threaddebugger.IThreadDebugger;
 import cn.dreamtobe.threaddebugger.ThreadDebugger;
 import cn.dreamtobe.threaddebugger.ThreadDebuggers;
-import okhttp3.OkHttpClient;
 
 /**
  * Created by Jacksgong on 12/17/15.
@@ -38,20 +33,7 @@ public class DemoApplication extends Application {
          * by below code, so please do not worry about performance.
          * @see FileDownloader#init(Context)
          */
-        FileDownloader.init(getApplicationContext(),
-                new FileDownloadHelper.OkHttpClientCustomMaker() { // is not has to provide.
-                    @Override
-                    public OkHttpClient customMake() {
-                        // just for OkHttpClient customize.
-                        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                        // you can set the connection timeout.
-                        builder.connectTimeout(15_000, TimeUnit.MILLISECONDS);
-                        // you can set the HTTP proxy.
-                        builder.proxy(Proxy.NO_PROXY);
-                        // etc.
-                        return builder.build();
-                    }
-                });
+        FileDownloader.init(getApplicationContext());
 
         // below codes just for monitoring thread pools in the FileDownloader:
         IThreadDebugger debugger = ThreadDebugger.install(
