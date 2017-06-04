707b258d9c39e3410f35425aee1e5d46d83d1ebd
Jacksgong <igzhenjie@gmail.com>
2017-06-04 22:40:31 +0800

    fix: fix the wrong timestamp used for sync on fetch data

--- library/src/main/java/com/liulishuo/filedownloader/download/FetchDataTask.java
+++ library/src/main/java/com/liulishuo/filedownloader/download/FetchDataTask.java
@@ -197,7 +197,7 @@ public class FetchDataTask {
 
 
             lastSyncBytes = currentOffset;
-            lastSyncTimestamp = currentOffset;
+            lastSyncTimestamp = now;
         }
     }
 
