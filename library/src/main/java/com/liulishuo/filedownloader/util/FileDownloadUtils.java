f4eee982bddc6c6590c1bfb6fd4adb8078813338
Michał Seroczyński <androidches@gmail.com>
2019-03-27 15:10:37 +0100

    Fix3

--- library/src/main/java/com/liulishuo/filedownloader/util/FileDownloadUtils.java
+++ library/src/main/java/com/liulishuo/filedownloader/util/FileDownloadUtils.java
@@ -859,6 +859,7 @@ public class FileDownloadUtils {
 
     public static boolean needMakeServiceForeground(Context context) {
         // FIXME return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isAppOnForeground(context);
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+//        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+      return false;
     }
 }
