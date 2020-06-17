6a2ccc74bb8ec962c4c7ca5c15f8d709cbb2ee93
z <z.y.j.1379@gmail.com>
2020-06-17 18:34:09 +0430

    delete log from mainActivity

--- demo/src/main/java/com/liulishuo/filedownloader/demo/MainActivity.java
+++ demo/src/main/java/com/liulishuo/filedownloader/demo/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
